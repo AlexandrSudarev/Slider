48e312bc70225e514e2bf7447616c178bff1ded6
AlexSudarev <aasudarev@sbercloud.ru>
2021-01-25 13:43:59 +0300

    UPD: options for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,80 +1,35 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: {
-        app: './src/index.js'
-    },
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, './dist'),
-        publicPath: '/dist'
-    },
-    module: {
-        rules: [{
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: '/node_modules',
-            }, {
-                test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            postcssOptions: {
-                                config: path.resolve(__dirname, "postcss.config.js"),
-                            },
-                        }
-                    }, {
-                        loader: 'postcss-loader',
-                        options: {
-                            postcssOptions: {
-                                config: path.resolve(__dirname, "postcss.config.js"),
-                            },
-                        }
-                    }, {
-                        loader: 'sass-loader',
-                        options: {
-                            postcssOptions: {
-                                config: path.resolve(__dirname, "postcss.config.js"),
-                            },
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            postcssOptions: {
-                                config: path.resolve(__dirname, "postcss.config.js"),
-                            },
-                        }
-                    }, {
-                        loader: 'postcss-loader',
-                        options: {
-                            postcssOptions: {
-                                config: path.resolve(__dirname, "postcss.config.js"),
-                            },
-                        }
-                    },
-                ]
-            }
-        ]
-    },
-    devServer: {
-        overlay: true,
-        open: true
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: '[name].css'
-        })
-    ]
+  entry: {
+    app: "./src/index.js",
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "/dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: "/node_modules",
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+  devServer: {
+    overlay: true,
+    open: true,
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+  ],
 };
